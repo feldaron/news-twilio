8a8baba435d15b0ce9021091deb31286d8091c27
feldaron <feldaron@gmail.com>
2025-06-29 20:29:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,6 @@ def bbc_headlines():
     return Response(twiml, mimetype='text/xml')
 
 if __name__ == "__main__":
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
